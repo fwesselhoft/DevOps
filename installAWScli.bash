367d076fa7e15d5a5a946e3adb1445893fcbe399
fwesselhoft <53708686+fwesselhoft@users.noreply.github.com>
2020-08-15 10:27:02 -0400

    Update installAWScli.bash

--- installAWScli.bash
+++ installAWScli.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /tmp/
+echo "Y" | sudo apt-get install zip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
